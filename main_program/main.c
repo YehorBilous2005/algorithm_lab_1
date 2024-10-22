9595e9e634913dcbacb8644b7ad6f695e3fe939a
Yehor <bilous801@gmail.com>
2024-10-22 14:51:35 +0300

    Yep

--- main_program/main.c
+++ main_program/main.c
@@ -9,135 +9,287 @@ struct NamesStruct{
     char *fileC[EXTRAFNUM];
 };
 
-void DivideFileA(struct NamesStruct *name, short fnum);
+void divideFileA(struct NamesStruct *name, short fnum);
 
-void CreateExtraFiles(struct NamesStruct *name, short fnum);
+void createExtraBFiles(struct NamesStruct *name, short fnum);
 
-void AllocateStructMemory(struct NamesStruct *name, short fnum);
+void allocateStructMemory(struct NamesStruct *name, short fnum);
 
-void FreeStructMemory(struct NamesStruct *name, short fnum);
+void freeStructMemory(struct NamesStruct *name, short fnum);
+
+void mergeFilesBUnoptimized(struct NamesStruct *name, short fnum);
 
 int main(){
     struct NamesStruct name;
-    AllocateStructMemory(&name, EXTRAFNUM);
-    CreateExtraFiles(&name, EXTRAFNUM);
-    DivideFileA(&name, EXTRAFNUM);
-    
-    FreeStructMemory(&name, EXTRAFNUM);
+    allocateStructMemory(&name, EXTRAFNUM);
+    createExtraBFiles(&name, EXTRAFNUM);
+    divideFileA(&name, EXTRAFNUM);
+    mergeFilesBUnoptimized(&name, EXTRAFNUM);
+
+    freeStructMemory(&name, EXTRAFNUM);
+    printf("\nABOBA_final");
     return 0;
 }
 
-void AllocateStructMemory(struct NamesStruct *name, short fnum){
-    // Dynamic memory allocation for digits
-    int *digits = (int *) calloc(1, sizeof(int));
-    if (!digits){
-        printf("Memory allocation failed for digits\n");
-        return 0;
+void allocateStructMemory(struct NamesStruct *name, short fnum){
+    int *digits = (int *) calloc(1, sizeof(int)); //memory allocation
+    if (digits == NULL){
+        printf("Error: memory allocation failed for digits\n");
+        return;
     }
-    *digits = (int)log10(EXTRAFNUM) + 2;
+    *digits = (int)log10(fnum) + 2;
 
-    // Dynamic memory allocation for name.files
-    for (int i = 0; i < EXTRAFNUM; i++){
-        name->fileB[i] = (char *) calloc(*digits, sizeof(char));
-        if (!name->fileB[i]){
-            printf("Memory allocation failed for names.fileB[%d]\n", i);
-            return 0;
+    for (int i = 0; i < fnum; i++){
+        name->fileB[i] = (char *) calloc(*digits, sizeof(char)); //memory allocation
+        if (name->fileB[i] == NULL){
+            printf("Error: memory allocation failed for names.fileB[%d]\n", i);
+            free(digits); // free memory
+            digits = NULL;
+            for (int j = 0; j < i; j++){
+                free(name->fileB[j]);
+                name->fileB[j] = NULL;
+            }
+            return;
         }
         name->fileC[i] = (char *) calloc(*digits, sizeof(char));
-        if (!name->fileC[i]){
-            printf("Memory allocation failed for names.fileC[%d]\n", i);
-            return 0;
+        if (name->fileC[i] == NULL){
+            printf("Error: memory allocation failed for names.fileC[%d]\n", i);
+            free(digits); // free memory
+            digits = NULL;
+            for (int j = 0; j < i; j++){
+                free(name->fileB[j]); 
+                name->fileB[j] = NULL;
+                free(name->fileC[j]);
+                name->fileC[j] = NULL;
+            }
+            return;
         }
     }
 
-    // Free dynamically allocated memory for digits
-    free(digits);
+    free(digits); // free memory
     digits = NULL;
 }
 
-void FreeStructMemory(struct NamesStruct *name, short fnum){
-    // Free dynamically allocated memory for name.files
+void freeStructMemory(struct NamesStruct *name, short fnum){
     for (int i = 0; i < EXTRAFNUM; i++){
-        free(name->fileB[i]);
+        free(name->fileB[i]); // free memory
         free(name->fileC[i]);
         name->fileB[i] = NULL;
         name->fileC[i] = NULL;
     }
 }
 
-void CreateExtraFiles(struct NamesStruct *name, short fnum){
+void createExtraBFiles(struct NamesStruct *name, short fnum){
     FILE *temp_file;
 
     for (int i = 0; i < fnum; i++){
         sprintf(name->fileB[i], "B%d.txt", i);
-        // Temp_file opening
-        temp_file = fopen(name->fileB[i], "w");
+        temp_file = fopen(name->fileB[i], "w"); //open file
 
-        if (temp_file == NULL) {
-            printf("Error creating file %s\n", name->fileB[i]);
-            // Optional free temp_file memory 
-            free(temp_file);
-
+        if (temp_file == NULL){
+            printf("Error: file %s creating failed\n", name->fileB[i]);
             return;
         }
         printf("File %s was created\n", name->fileB[i]);
-        // Temp_file closing
-        fclose(temp_file);
+
+        fclose(temp_file); // close file
         temp_file = NULL;
 
         sprintf(name->fileC[i], "C%d.txt", i);
-        // Temp_file opening
-        temp_file = fopen(name->fileC[i], "w");
+        temp_file = fopen(name->fileC[i], "w"); //open file
 
-        if (temp_file == NULL) {
-            printf("Error creating file %s\n", name->fileC[i]);
-            // Optional free temp_file memory 
-            free(temp_file);
-
+        if (temp_file == NULL){
+            printf("Error: file %s creating failed\n", name->fileC[i]);
             return;
         }
         printf("File %s was created\n", name->fileC[i]);
-        // Temp_file closing
-        fclose(temp_file);
+
+        fclose(temp_file); // close file
         temp_file = NULL;
     }
 }
 
-void DivideFileA(struct NamesStruct *name, short fnum){
-    // File_a opening
-    FILE *file_a = fopen("D:\\Education\\Algorithms\\lab1\\text_files\\A.txt", "r");
-    if (!file_a){
-        printf("Error opening file A.txt\n");
+void divideFileA(struct NamesStruct *name, short fnum){
+    FILE *file_a = fopen("D:\\Education\\Algorithms\\lab1\\text_files\\A.txt", "r"); // open file
+    if (file_a == NULL){
+        printf("Error: file A.txt opening failed\n");
         return;
     }
 
-    // Dynamic memory allocation for x
-    int *x = (int *) calloc(1, sizeof(int));
+    int *x = (int *) calloc(1, sizeof(int)); // memory allocation
 
     FILE *temp_file;
-    while (file_a){
+    while (!feof(file_a)){
         for (int i = 0; i < fnum; i++){
             if (fscanf(file_a, "%d", x) != EOF){
-                // Temp_file opening
-                temp_file = fopen(name->fileB[i], "a");
-
-                if (!temp_file){
-                    printf("Error opening file name.fileB[%d]\n", i);
+                temp_file = fopen(name->fileB[i], "a"); // open file
+                if (temp_file == NULL){
+                    printf("Error: file name.fileB[%d] opening failed\n", i);
+                    free(x);
+                    fclose(file_a);
                     return;
                 }
                 fprintf(temp_file, "%d\n", *x);
-                // Temp_file closing
-                fclose(temp_file);
+                fclose(temp_file); // close file
             }
             else{
                 break;
             }  
         }
     }
-    // Free dynamically allocated memory for x
-    free(x);
+
+    free(x); // memory free
     x = NULL;
 
-    // File_a closing
-    fclose(file_a);
+    fclose(file_a); // close file
+}
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high){
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+void quicksort(int arr[], int low, int high){
+    if (low < high){
+        int pi = partition(arr, low, high);
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
+
+void mergeFilesBUnoptimized(struct NamesStruct *name, short fnum){
+    FILE *fileB[fnum];
+    FILE *fileC = NULL;
+
+    int *arr = (int *) calloc(fnum, sizeof(int)); // memory allocation
+    if (arr == NULL){
+        printf("Error: memory allocation failed for arr\n");
+        return;
+    }
+
+    for (int i = 0; i < fnum; i++){
+        fileB[i] = fopen(name->fileB[i], "r"); // open files b
+    }
+
+    int *index = (int *) calloc(1, sizeof(int)); // memory allocation
+    *index = 0; 
+
+    while (!feof(fileB[0])){
+        for (int i = 0; i < fnum; i++){
+            if (fileB[i] == NULL) {
+                printf("Failed to open file: %s\n", name->fileB[i]);
+                free(index);
+                free(arr);
+                for (int i = 0; i < fnum; i++){
+                    fclose(fileB[i]);
+                    fileB[i] = NULL;
+                }
+                return;
+            }
+            if (fscanf(fileB[i], "%d", &arr[i]) != 1){
+                printf("Error reading from file: %s\n", name->fileB[i]);
+                fileC = fopen(name->fileC[*index], "a+");
+                for (int j = 0; j < i; j++){
+                    fprintf(fileC, "%d\n", arr[j]);
+                }
+                fclose(fileC);
+                free(index);
+                free(arr);
+                for (int i = 0; i < fnum; i++){
+                    fclose(fileB[i]);
+                }
+                return;
+            }
+        }
+
+        quicksort(arr, 0, fnum - 1);
+        for (int i = 0; i < fnum; i++){
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+
+        fileC = fopen(name->fileC[*index], "a+"); // open file
+        if (fileC == NULL){
+            printf("Failed to open file for reading: %s\n", name->fileC[*index]);
+            free(index);
+            free(arr);
+            for (int i = 0; i < fnum; i++){
+                fclose(fileB[i]);
+                fileB[i] = NULL;
+            }
+            return;
+        }
+        // printf("%d\n", *index);
+
+        int *temp_num = (int *) calloc(1, sizeof(int));  // memory allocation
+        FILE *temp_file = fopen("tempFile", "w"); // open file
+        fseek(fileC, 0, SEEK_SET);
+        fscanf(fileC, "%d", temp_num);
+        if (feof(fileC)){
+            for (int i = 0; i < fnum; i++){
+                fprintf(fileC, "%d\n", arr[i]);
+            }
+        }
+        else{
+            for (int i = 0; i < fnum; i++){
+                while (!feof(fileC)){
+                    if (*temp_num <= arr[i]){
+                        fprintf(temp_file, "%d\n", *temp_num);
+                        fscanf(fileC, "%d", temp_num);
+                    }
+                    else{
+                        fprintf(temp_file, "%d\n", arr[i]);
+                        break;
+                    }
+                }
+                if (feof(fileC)){
+                    fprintf(temp_file, "%d\n", arr[i]);
+                }
+            }    
+            while (fscanf(fileC, "%d", temp_num) == 1){
+                fprintf(temp_file, "%d\n", *temp_num);
+            }
+            fclose(fileC);
+            fclose(temp_file);
+
+            temp_file = fopen("tempFile", "r");
+            fileC = fopen(name->fileC[*index], "w");
+            while (fscanf(temp_file, "%d", temp_num) == 1){
+                fprintf(fileC, "%d\n", *temp_num);
+            }
+        }
+        free(temp_num); // free memory
+        temp_num = NULL;
+        fclose(temp_file); // close file
+    
+        fclose(fileC); // close file
+
+        (*index)++;
+        if (*index >= fnum){
+            *index = 0;
+        }
+    }
+
+    for (int i = 0; i < fnum; i++){
+        fclose(fileB[i]);
+    }
+
+    free(arr); // free memory
+    arr = NULL;
+    free(index);
+    index = NULL;
 }
